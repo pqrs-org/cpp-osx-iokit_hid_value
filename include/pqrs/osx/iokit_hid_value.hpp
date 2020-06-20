21b9161c7cfb872bf51b042cd2bf4ca93f13336f
Takayama Fumihiko <tekezo@pqrs.org>
2020-06-20 12:14:42 +0900

    Version 3.3.0

--- include/pqrs/osx/iokit_hid_value.hpp
+++ include/pqrs/osx/iokit_hid_value.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_hid_value v3.2
+// pqrs::osx::iokit_hid_value v3.3
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
